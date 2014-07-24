8da782a59072d1b0500d8cc5bd02a92b36bd6e92
alexperezpaya <alejandro@perezpaya.net>
2014-07-24 03:45:30 +0200

    Simple tests

--- index.js
+++ index.js
@@ -3,9 +3,3 @@ var parser = require('./lib/parser')
 module.exports = {
 	getPlatforms: parser.getPlatforms
 }
-
-parser.getPlatforms(function (err, platforms) {
-
-	console.log(err, platforms);
-
-})
---
+++ test.js
@@ -0,0 +1,7 @@
+var coolrom = require('./index')
+
+coolrom.getPlatforms(function (err, platforms) {
+
+	console.log(err, platforms);
+
+})
